cb4bf7df288f3d5150fccb4e817796f63b8c56e1
g0at1 <michal.lendzion@vp.pl>
2023-02-01 15:21:26 +0100

    Added method to find librarian by email.

--- src/main/java/com/example/demo/librarian/LibrarianRepository.java
+++ src/main/java/com/example/demo/librarian/LibrarianRepository.java
@@ -1,7 +1,13 @@
 package com.example.demo.librarian;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface LibrarianRepository
         extends JpaRepository<Librarian, Integer> {
+
+    @Query("SELECT l FROM Librarian l WHERE l.email = ?1")
+    Optional<Librarian> findLibrarianByEmail(String email);
 }
